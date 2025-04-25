9b70ef39947d843c1b745ab725678f3961398c78
Yeison Bernal <lopezb1069@gmail.com>
2025-04-25 18:10:40 +1000

    Migration data added

---
+++ SixMinApi/Migrations/20250425080301_SeedInitialData.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SixMinApi.Migrations
+{
+    public partial class SeedInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Commands",
+                columns: new[] {"HowTo", "CommandLine", "Platform" },
+                values: new object[,]
+                {
+                    {"List files", "ls -la", "Linux" },
+                    {"Print working dir", "pwd", "Linux" },
+                    {"Make directory", "mkdir new_folder", "Linux" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
